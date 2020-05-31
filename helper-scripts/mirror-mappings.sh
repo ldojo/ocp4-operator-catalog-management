97788d94fcbd9acf4cd768b2be102f2f284aea8d
Cloud User <ec2-user@ip-172-31-33-22.us-east-2.compute.internal>
2020-05-31 20:51:52 +0000

    coalesce all mappings into one file

--- helper-scripts/mirror-mappings.sh
+++ helper-scripts/mirror-mappings.sh
@@ -5,9 +5,13 @@ set -x
 
 REG_CREDS=${XDG_RUNTIME_DIR}/containers/auth.json
 
+rm operatorhub-mapping.txt
+
 for CATALOG in redhat-operators certified-operators community-operators
 do
 	 CATALOG_IMAGE=${REPO_URL}/docker-local/${CATALOG}:v4.4
 	oc adm catalog mirror --manifests-only ${REPO_URL}/docker-local/${CATALOG}:v4.4 test.io --insecure -a ${REG_CREDS} 
+	cat ${CATALOG}-manifests/mapping.txt | sort -u >> operatorhub-mapping.txt
 done
+
 
